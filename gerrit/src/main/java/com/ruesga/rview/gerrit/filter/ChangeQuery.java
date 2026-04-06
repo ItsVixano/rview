5b73b583b3606604f0f1d35228aded810669f1b3
Giovanni Ricca <giovanniricca@proton.me>
2026-04-06 12:20:22 +0200

    [HAX] rview: Add Gerrit 3.10 dashboard filters
    The check must be fixed properly instead (3.70 > 3.10)
    
    Change-Id: I515ea89a77727cd939bc1429600232a133831802

--- gerrit/src/main/java/com/ruesga/rview/gerrit/filter/ChangeQuery.java
+++ gerrit/src/main/java/com/ruesga/rview/gerrit/filter/ChangeQuery.java
@@ -80,7 +80,7 @@ public class ChangeQuery extends ComplexQuery<ChangeQuery> {
             null, null, null, null, null, null, null, null,
             null, null, null, null, null, null,
             null, 2.14d, 2.14d, 2.14d, 2.14d, 2.15d,
-            2.15d, 3.0d, 3.0d, 3.0d, 3.0d, 3.7d
+            2.15d, 3.0d, 3.0d, 3.0d, 3.0d, 3.7d, 3.10d
     };
 
     public static final Double[] UNSUPPORTED_FROM_VERSION = {
@@ -91,7 +91,7 @@ public class ChangeQuery extends ComplexQuery<ChangeQuery> {
             null, null, null, null, null, null, null, null,
             null, null, null, null, null, null,
             2.15d, null, null, null, null, null,
-            null, null, null, null, null, null
+            null, null, null, null, null, null, null
     };
 
     public ChangeQuery age(TimeUnit unit, int value) {
