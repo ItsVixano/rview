ItsVixano/rview

commit bcdafd63ca9369b9fde6ebdaddaa7104128165de

rview: Only show the preferred email
Change-Id: I5f8a1420eb4fbdb46ad6699b439e016ae29f8ad5

app/src/main/java/com/ruesga/rview/misc/Formatter.java
@@ -689,16 +689,7 @@ public class Formatter {
             return;
         }
 
-        StringBuilder emails = new StringBuilder(account.email);
-        if (account.secondaryEmails != null) {
-            emails.append(" <b>")
-                    .append(v.getContext().getString(R.string.account_details_emails_preferred))
-                    .append("</b>");
-            for (String email : account.secondaryEmails) {
-                emails.append("<br/>").append(email);
-            }
-        }
-        v.setText(Html.fromHtml(emails.toString()));
+        v.setText(account.email);
     }
 
     @BindingAdapter("projectStatus")

commit 46aa97c8066134e69138fe5aa1fdff6f7c45fdb3

rview: Remove `secondary_emails` logic from `app`
Grepping for those is quite useless now given we are now
showing the preferred mail only (to match with web ui).

Fixes: bcdafd63ca93 ("rview: Only show the preferred email")

Change-Id: I316c47d37c9d8be5d0151acdd2b1e641a0eafb69

app/src/main/java/com/ruesga/rview/fragments/AccountStatsPageFragment.java
@@ -116,28 +116,6 @@ public class AccountStatsPageFragment extends StatsPageFragment<AccountDetailInf
                     }
                 }
 
-                // Fetch secondary emails (only for self account)
-                Account account = Preferences.getAccount(getContext());
-                if (mCachedAccount.accountId == account.mAccount.accountId &&
-                        mCachedAccount.secondaryEmails == null) {
-                    try {
-                        List<EmailInfo> emails = api.getAccountEmails(GerritApi.SELF_ACCOUNT)
-                                .blockingFirst();
-                        if (emails != null) {
-                            List<String> secondaryEmails = new ArrayList<>();
-                            for (EmailInfo email : emails) {
-                                if (!email.pendingConfirmation && !email.preferred) {
-                                    secondaryEmails.add(email.email);
-                                }
-                            }
-                            mCachedAccount.secondaryEmails =
-                                    secondaryEmails.toArray(new String[0]);
-                        }
-                    } catch (Exception ex) {
-                        // Ignore
-                        ex.printStackTrace();
-                    }
-                }
                 return mCachedAccount;
             });
     }

app/src/main/java/com/ruesga/rview/misc/Formatter.java
@@ -682,8 +682,8 @@ public class Formatter {
                 ? R.drawable.ic_group : R.drawable.ic_person);
     }
 
-    @BindingAdapter("accountEmails")
-    public static void toAccountEmails(TextView v, AccountDetailInfo account) {
+    @BindingAdapter("accountEmail")
+    public static void toAccountEmail(TextView v, AccountDetailInfo account) {
         if (account == null || account.email == null) {
             v.setText(null);
             return;

app/src/main/res/layout/account_details_view.xml
@@ -172,7 +172,7 @@
                 <com.ruesga.rview.widget.StyleableTextView
                     android:layout_width="@dimen/label_min_width"
                     android:layout_height="wrap_content"
-                    android:text="@string/account_details_emails"
+                    android:text="@string/account_details_email"
                     android:textAppearance="?android:attr/textAppearanceSmall"
                     android:textColor="?android:attr/textColorPrimary"
                     android:textSize="12sp" />
@@ -181,7 +181,7 @@
                     android:layout_height="wrap_content"
                     android:layout_marginStart="5dp"
                     android:layout_marginLeft="5dp"
-                    app:accountEmails="@{model}"
+                    app:accountEmail="@{model}"
                     android:textAppearance="?android:attr/textAppearanceSmall"
                     android:textColor="?android:attr/textColorSecondary"
                     android:visibility="@{model == null ? View.GONE : View.VISIBLE}"

app/src/main/res/values/strings.xml
@@ -565,8 +565,7 @@
     <string name="account_details_username">Username</string>
     <string name="account_details_display_name">Display Name</string>
     <string name="account_details_name">Name</string>
-    <string name="account_details_emails">Emails</string>
-    <string name="account_details_emails_preferred"> (preferred)</string>
+    <string name="account_details_email">Email</string>
     <string name="account_details_registered_on">Registered</string>
     <string name="account_details_status">Status</string>
     <string name="account_details_active">Active</string>